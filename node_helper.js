17fce0ef6e4bc8c4abfa0a6e5ceb5e318e4506c6
Paul Ruiz <ptruiz@google.com>
2025-03-17 14:31:30 -0600

    a

--- node_helper.js
+++ node_helper.js
@@ -1,10 +1,8 @@
 const NodeHelper = require("node_helper");
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-
 
 module.exports = NodeHelper.create({
   socketNotificationReceived: async function (notification, payload) {
-    if (notification === "GENERATE_GEMINI_TEXT") {
+    if (notification === "GENERATE_TEXT") {
       const apiKey = payload.apiKey;
       const prompt = "write a story about a magic mirror";
       const modelName = payload.model || "gemini-2.0-flash"; // Default to gemini-pro if no model is specified
@@ -22,20 +20,21 @@ module.exports = NodeHelper.create({
       }
 
       try {
+        const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-        // const ai = new GoogleGenAI({ apiKey: apiKey });
+        const ai = new GoogleGenAI({ apiKey: apiKey });
 
-        // const model = genAI.getGenerativeModel({ model: modelName })
+        const model = genAI.getGenerativeModel({ model: modelName })
 
-        // const response = await ai.models.generateContent({
-        //   model: "gemini-2.0-flash",
-        //   contents: "Write a story about a magic backpack.",
-        // })
+        const response = await ai.models.generateContent({
+          model: "gemini-2.0-flash",
+          contents: "Write a story about a magic backpack.",
+        })
 
-        // console.log(response.text);
-        // const text = response.text();
+        console.log(response.text);
+        const text = response.text();
 
-        this.sendSocketNotification("GEMINI_TEXT_RESULT", { text: "TEST" });
+        this.sendSocketNotification("NOTIFICATION_GENERATE_TEXT", { text: text });
       } catch (error) {
         console.error("Error generating text with Gemini:", error);
         this.sendSocketNotification("GEMINI_TEXT_ERROR", { error: error.message });

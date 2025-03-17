738c19114b8f7725c060558a42ce2f4c75cafe08
Paul Ruiz <ptruiz@google.com>
2025-03-17 13:58:15 -0600

    api

--- node_helper.js
+++ node_helper.js
@@ -2,10 +2,9 @@ const NodeHelper = require("node_helper")
 
 module.exports = NodeHelper.create({
 
-  import { GoogleGenAI } from "@google/genai";
-
 
   async socketNotificationReceived(notification, payload) {
+
     if (notification === "GET_RANDOM_TEXT") {
       const amountCharacters = payload.amountCharacters || 10
       const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")

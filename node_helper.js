0ac4e14bf0e8c8868dbce41a7bd73c7ae8924fe1
Paul Ruiz <ptruiz@google.com>
2025-03-17 14:36:21 -0600

    a

--- node_helper.js
+++ node_helper.js
@@ -1,44 +1,15 @@
-const NodeHelper = require("node_helper");
+const NodeHelper = require("node_helper")
 
 module.exports = NodeHelper.create({
-  socketNotificationReceived: async function (notification, payload) {
-    if (notification === "GENERATE_TEXT") {
-      const apiKey = payload.apiKey;
-      const prompt = "write a story about a magic mirror";
-      const modelName = payload.model || "gemini-2.0-flash"; // Default to gemini-pro if no model is specified
 
-      if (!apiKey) {
-        console.error("Gemini API key is missing.");
-        this.sendSocketNotification("GEMINI_TEXT_ERROR", { error: "API key missing" });
-        return;
-      }
-
-      if (!prompt) {
-        console.error("Gemini prompt is missing.");
-        this.sendSocketNotification("GEMINI_TEXT_ERROR", { error: "Prompt missing" });
-        return;
-      }
-
-      try {
-        // const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-        // const ai = new GoogleGenAI({ apiKey: apiKey });
-
-        // const model = genAI.getGenerativeModel({ model: modelName })
-
-        // const response = await ai.models.generateContent({
-        //   model: "gemini-2.0-flash",
-        //   contents: "Write a story about a magic backpack.",
-        // })
-
-        // console.log(response.text);
-        // const text = response.text();
-
-        this.sendSocketNotification("NOTIFICATION_GENERATE_TEXT", { text: "TEST TEST" });
-      } catch (error) {
-        console.error("Error generating text with Gemini:", error);
-        this.sendSocketNotification("GEMINI_TEXT_ERROR", { error: error.message });
-      }
+  async socketNotificationReceived(notification, payload) {
+    if (notification === "GET_RANDOM_TEXT") {
+      const amountCharacters = payload.amountCharacters || 10
+      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
+      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
+    }
+    if( notification === "GENERATE_TEXT") {
+      this.sendSocketNotification("NOTIFICATION_GENERATE_TEXT", { text: "Generated text" })
     }
   },
-});
+})

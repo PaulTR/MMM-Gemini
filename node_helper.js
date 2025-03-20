352e2e306e690d5fa5e83a0348ebdb8a61c7f97b
Paul Ruiz <ptruiz@google.com>
2025-03-20 15:03:40 -0600

    a

--- node_helper.js
+++ node_helper.js
@@ -35,17 +35,20 @@ module.exports = NodeHelper.create({
         contents: "Please generate an image of a magical red dragon in a fantasy style",
       });
 
-      for (const part of  response.response.candidates[0].content.parts) {
-        // Based on the part type, either show the text or save the image
-        if (part.text) {
-          console.log(part.text);
-        } else if (part.inlineData) {
-          const imageData = part.inlineData.data;
-          const buffer = Buffer.from(imageData, 'base64');
-          fs.writeFileSync('gemini-native-image.png', buffer);
-          console.log('Image saved as gemini-native-image.png');
-        }
-      }
+      const responseString = JSON.stringify(response, null, 2);
+      console.log('Received message from the server:\n', responseString);
+
+      // for (const part of  response.response.candidates[0].content.parts) {
+      //   // Based on the part type, either show the text or save the image
+      //   if (part.text) {
+      //     console.log(part.text);
+      //   } else if (part.inlineData) {
+      //     const imageData = part.inlineData.data;
+      //     const buffer = Buffer.from(imageData, 'base64');
+      //     fs.writeFileSync('gemini-native-image.png', buffer);
+      //     console.log('Image saved as gemini-native-image.png');
+      //   }
+      // }
 
       this.sendSocketNotification("NOTIFICATION_GENERATE_IMAGE", { text: "image" });
     }

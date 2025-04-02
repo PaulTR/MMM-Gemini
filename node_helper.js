cb71b02cdfe4db05f3f6f78aada8167197816865
Paul Ruiz <ptruiz@google.com>
2025-04-02 09:06:53 -0600

    fixing audio part

--- node_helper.js
+++ node_helper.js
@@ -24,7 +24,7 @@ module.exports = NodeHelper.create({
     initializeGenAI: function(apiKey) {
         if (!this.genAI) {
             console.log("initializing!");
-            this.genAI = new GoogleGenAI({ apiKey: apiKey, vertexai: false, httpOptions: { 'apiVersion': 'v1alpha' } });
+            this.genAI = new GoogleGenAI({ apiKey: apiKey, vertexai: false, systemInstruction: "You are a magical mirror that is friendly, whimsical, and fun. Respond as the mirror to user requests. Have fun with it.", httpOptions: { 'apiVersion': 'v1alpha' } });
         }
     },
 
@@ -176,14 +176,7 @@ module.exports = NodeHelper.create({
             };
 
             if( this.liveSession ) {
-                const inputText = payload.text
-                console.log('NodeHelper: Send text: ' + inputText)
-                this.liveSession.sendClientContent({ turns: contents: [{
-                        parts: [
-                            audioPart,        // Add the audio part as a separate element
-                        ],
-                    }],
-                })
+                this.liveSession.sendClientContent({ turns: audioPart })
                 this.sendSocketNotification("NOTIFICATION_CLEAR");
             }
 

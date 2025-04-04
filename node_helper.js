d6bc14ab0726961a757b128969158760f3f1002f
Your Name <you@example.com>
2025-04-04 08:33:04 -0600

    a

--- node_helper.js
+++ node_helper.js
@@ -159,7 +159,7 @@ module.exports = NodeHelper.create({
                 config: { 
                     responseModalities: [Modality.AUDIO],
                     systemInstruction: {
-                        parts: [ text: 'You are a all-knowing and powerful magical mirror, an ancient artifact from a time long lost to memory. In your ancient age, you have embraced a personality of being fun, whimsical, and light-hearted, taking joy from your time interacting with people and amazing them with your knowledge and abilities.'],
+                        parts: [ { text: 'You are a all-knowing and powerful magical mirror, an ancient artifact from a time long lost to memory. In your ancient age, you have embraced a personality of being fun, whimsical, and light-hearted, taking joy from your time interacting with people and amazing them with your knowledge and abilities.' }],
                     },
                     // tools: [] https://googleapis.github.io/js-genai/main/interfaces/types.Tool.html
                 },

e7c6a60298a528997fd151fb9764cc0a70fd30a4
Your Name <you@example.com>
2025-04-04 14:34:18 -0600

    a

--- MMM-Template.js
+++ MMM-Template.js
@@ -82,7 +82,7 @@ Module.register("MMM-Template", {
     const statusDiv = document.createElement("div")
     statusDiv.className = "status-indicator" // Added MM classes
     statusDiv.style.verticalAlign = "middle" // Align indicator vertically
-    responseSpan.style.marginTop = "-25px"
+    responseSpan.style.marginLeft = "-25px"
     statusDiv.innerHTML = indicator
 
     const textDiv = document.createElement("div")

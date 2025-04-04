0ab7e04ab0c3a8e7a784264f1602ad7de7fa6f25
Your Name <you@example.com>
2025-04-04 10:29:36 -0600

    test audio again

--- node_helper.js
+++ node_helper.js
@@ -157,8 +157,8 @@ module.exports = NodeHelper.create({
                 },
                 // https://googleapis.github.io/js-genai/main/interfaces/types.LiveConnectConfig.html
                 config: { 
-                    // responseModalities: [Modality.AUDIO],
-                    responseModalities: [Modality.TEXT],
+                    responseModalities: [Modality.AUDIO],
+                    // responseModalities: [Modality.TEXT],
                     systemInstruction: {
                         parts: [ { text: 'You are a all-knowing and powerful magical mirror, an ancient artifact from a time long lost to memory. In your ancient age, you have embraced a personality of being fun, whimsical, and light-hearted, taking joy from your time interacting with people and amazing them with your knowledge and abilities.' }],
                     },

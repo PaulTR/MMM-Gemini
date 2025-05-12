51a3ca2035be5653f1e5f16158309e851a653a1c
Your Name <you@example.com>
2025-05-12 13:31:05 -0600

    a

--- node_helper.js
+++ node_helper.js
@@ -148,11 +148,11 @@ module.exports = NodeHelper.create({
                 
                 config: {
                     responseModalities: [Modality.AUDIO],
-                    // realtimeInputConfig: {
-                    //     automaticActivityDetection: {
-                    //         startOfSpeechSensitivity: StartSensitivity.START_SENSITIVITY_LOW
-                    //     },
-                    // },
+                    realtimeInputConfig: {
+                        automaticActivityDetection: {
+                            startOfSpeechSensitivity: StartSensitivity.START_SENSITIVITY_LOW
+                        },
+                    },
                     sessionResumption: {
                         handle: GEMINI_SESSION_HANDLE,
                         transparent: true,
